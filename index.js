db032174d7082ef107fde20a2ecd8c5e3d1d33f4
Erin Hatherell <93974950+emhat1@users.noreply.github.com>
2022-02-20 15:23:16 +1100

    Initialupload

---
+++ index.js
@@ -0,0 +1,10 @@
+// Setting up dependencies
+const fs = require('fs');
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+
+// Conection of server and database
+
+// Initial enquiry
+
+
