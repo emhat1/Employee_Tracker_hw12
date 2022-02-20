71f72f98b9c73cf9b8e7f2dd8327f91232e7d188
Erin H <e.hatherell@gmail.com>
2022-02-20 20:24:13 +1100

    Addition of query choices

--- index.js
+++ index.js
@@ -12,7 +12,55 @@ const db = mysql.createConnection({
     database: 'employees_db'
 })
 
-
 // Initial enquiry
+
+const beginPrompt = () => {
+    inquirer.prompt({
+        name: 'action',
+        type: 'list',
+        message: 'Welcome to the employee database.  Please select an option below:',
+        choices: [
+            'View all employees',
+            'View all departments',
+            'View all roles',
+            'Add an employee',
+            'Add a department',
+            'Add a role',
+            'Update an employee role',
+            'Delete an employee',
+            'EXIT'
+        ]
+    })
+    .then((answers) => {
+        switch (answers.beginPrompt) {
+            case 'View all employees':
+                viewAllEmployees();
+                break;
+            case 'Add an employee':
+                addEmployee();
+                break;
+            case 'View all departments':
+                viewAllDepartments();
+                break;
+            case 'View all roles':
+                viewRoles();
+                break;
+            case 'Add a department':
+                addDepartment();
+                break;
+            case 'Add a role':
+                addRole();
+                break;
+            case 'Update employee role':
+                updateRole();
+                break;
+            case 'Delete an employee':
+                deleteEmployee();
+                break;
+            case 'Quit': 
+                quit();
+        }
+    });
+};
 
 
